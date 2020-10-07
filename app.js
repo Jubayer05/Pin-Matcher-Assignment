a5e2ca42b69d49e5d40057d49691e68d4bda2ec3
Jubayer <programmer.jubayer@gmail.com>
2020-10-08 01:47:17 +0600

    last one task is still rest

---
+++ app.js
@@ -0,0 +1,64 @@
+// UI variables
+const wrongNotify = document.getElementById("wrong_notify"),
+        rightNotify = document.getElementById("right_notify"),
+        generateNum = document.getElementById("generate_num"),
+        clearAll = document.getElementById("clearAll"),
+        keyboardInput = document.getElementById("keyboard_num"),
+        generateBtn = document.querySelector(".generate-btn"),
+        submitBtn = document.querySelector(".submit-btn");
+        
+
+        wrongNotify.style.display = "none";
+        rightNotify.style.display = "none";
+
+// Show Random Number
+generateBtn.addEventListener("click", randomNum);
+
+// Click Number and show it in the box
+function dis(val) {
+    keyboardInput.value+=val;
+}
+
+
+function randomNum() {
+    const randomNum = Math.round(Math.random()*10000);
+    if(randomNum > 999 && randomNum < 10000){
+        generateNum.value = randomNum;
+    }
+}
+
+submitBtn.addEventListener("click", checkPin);
+
+// Clear Input
+function clearInput() {
+    keyboardInput.value = "";
+}
+
+// BackSpace
+function backSpace() {
+    let keyBack = keyboardInput.value;
+    keyboardInput.value = keyBack.substring(0, keyBack.length - 1); 
+}
+
+
+// Check If the number is match or not
+function checkPin () {
+    if (generateNum.value === keyboardInput.value) {
+        rightNotify.style.display = "block";
+        document.querySelector("body").style.background = "green";
+        keyboardInput.setAttribute('disabled', true);
+        
+        setTimeout(function() {
+            rightNotify.style.display = "none";
+        }, 3000)
+        
+    } else {
+            wrongNotify.style.display = "block";
+            document.querySelector("body").style.background = "red";
+        setTimeout(function() {
+            wrongNotify.style.display = "none";
+            document.querySelector("body").style.background = "black";
+        }, 3000)
+        
+    }
+}
